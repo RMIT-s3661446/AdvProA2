e44e30ee2eb5a22632c7da7c13d0403a2ee56195
RMIT-s3661446 <s3661446@student.rmit.edu.au>
2024-05-04 20:26:36 +1000

    commit test

--- ftt.cpp
+++ ftt.cpp
@@ -6,10 +6,19 @@
  * data, display the main menu, and handles the processing of options. 
  * Make sure free memory and close all files before exiting the program.
  **/
+
+
+
+ //commit check sign your name here and commit:
+//Mikhail Vasilchenko
+
 int main(int argc, char **argv)
 {
     /* validate command line arguments */
     // TODO
+
+
+    
 
     std::cout << "Just a test, nothing implemented yet!" << std::endl;
     
