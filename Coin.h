d04de8e61089e6538d8c3d92cb9161ebf59d4324
RMIT-s3661446 <s3661446@student.rmit.edu.au>
2024-05-19 12:50:41 +1000

    Added $50 note

--- Coin.h
+++ Coin.h
@@ -8,14 +8,14 @@
 // Keeping the original enum structure
 enum Denomination {
     FIVE_CENTS, TEN_CENTS, TWENTY_CENTS, FIFTY_CENTS, ONE_DOLLAR, 
-    TWO_DOLLARS, FIVE_DOLLARS, TEN_DOLLARS, TWENTY_DOLLARS
+    TWO_DOLLARS, FIVE_DOLLARS, TEN_DOLLARS, TWENTY_DOLLARS, FIFTY_DOLLARS
 };
 
 // Maps enum to its value in cents
 static const std::map<Denomination, int> DenominationValues = {
     {FIVE_CENTS, 5}, {TEN_CENTS, 10}, {TWENTY_CENTS, 20}, {FIFTY_CENTS, 50},
     {ONE_DOLLAR, 100}, {TWO_DOLLARS, 200}, {FIVE_DOLLARS, 500},
-    {TEN_DOLLARS, 1000}, {TWENTY_DOLLARS, 2000}
+    {TEN_DOLLARS, 1000}, {TWENTY_DOLLARS, 2000}, {FIFTY_DOLLARS, 5000}
 };
 
 class CoinManager {
